8994beed612cda0747483dd65c799e3532cbc4b1
Ore Deru <93195185+otmb1@users.noreply.github.com>
2023-09-19 21:48:20 +0100

    Add files via upload

---
+++ toucantech_demo/app/Models/members_model.php
@@ -0,0 +1,40 @@
+<?php
+
+class MembersModel {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    public function addMember($name, $email, $school_id) {
+        $name = $this->conn->real_escape_string($name);
+        $email = $this->conn->real_escape_string($email);
+        
+        $sql = "INSERT INTO members (member_name, member_email, school_id) VALUES ('$name', '$email', '$school_id')";
+        
+        if ($this->conn->query($sql) === TRUE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getMembersBySchool($school_id) {
+        $school_id = $this->conn->real_escape_string($school_id);
+        
+        $sql = "SELECT * FROM members WHERE school_id = '$school_id'";
+        $result = $this->conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+            $members = [];
+            while ($row = $result->fetch_assoc()) {
+                $members[] = $row;
+            }
+            return $members;
+        } else {
+            return [];
+        }
+    }
+}
+?>
