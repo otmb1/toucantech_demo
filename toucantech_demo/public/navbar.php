759791c1b68f5947032f154ca129b40ec85c19f9
Ore Deru <93195185+otmb1@users.noreply.github.com>
2023-09-20 21:10:36 +0100

    Add files via upload

--- toucantech_demo/public/navbar.php
+++ toucantech_demo/public/navbar.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="/toucantech_demo/public/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
@@ -12,21 +12,22 @@
             
             <div>
                 <ul class="navbar-nav">
-					<li class="nav-item">
+					<li class="nav-item px-2">
                         <a class="nav-link" href="/toucantech_demo/public/">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/toucantech_demo/app/Views/members/list_members.php">List Members</a>
-                    </li>
-                    <li class="nav-item">
+                    <li class="nav-item px-2">
                         <a class="nav-link" href="/toucantech_demo/app/Views/members/add_members.php">Add Member</a>
                     </li>
-					<li class="nav-item">
-                        <a class="nav-link" href="/toucantech_demo/app/Views/schools/list_schools.php">List Schools</a>
-                    </li>
-                    <li class="nav-item">
+					<li class="nav-item px-2">
                         <a class="nav-link" href="/toucantech_demo/app/Views/schools/add_schools.php">Add School</a>
                     </li>
+					<li class="nav-item px-2">
+                        <a class="nav-link" href="/toucantech_demo/app/Views/members/list_members.php">List Members</a>
+                    </li>
+					<li class="nav-item px-2">
+                        <a class="nav-link" href="/toucantech_demo/app/Views/schools/list_schools.php">List Schools</a>
+                    </li>
+                    
                 </ul>
             </div>
         </div>
